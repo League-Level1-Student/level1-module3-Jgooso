7772c8c39525d316fb27f3decdb413fa605f2308
League Student <league@iMac-18.attlocal.net>
2020-02-08 11:41:59 -0800

    finihsed module 3

--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -9,12 +9,14 @@ import java.io.IOException;
 import javax.swing.JApplet;
 
 public class CowTimer {
-
+	private int minutes;
 	/* 1. Make a constructor for the CowTimer class that initializes the minutes variable */
-	
+	public CowTimer(int min) {
+		this.minutes = min;
+	}
 	/* 4. Complete the main method of the CowTimerRunner class */
 
-	private int minutes;
+	
 
 	public void setTime(int minutes) {
 		this.minutes = minutes;
@@ -26,12 +28,18 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for the number of minutes
 		 * using Thread.sleep(int milliseconds). 
 		 */
+		for(int i = minutes; i > 0; i --) {
+			System.out.print(minutes);
+			minutes = minutes - 1;
+			Thread.sleep(1000);
+		}
 
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
 		 */
+		playSound("moo.wav");
 
 	}
 
